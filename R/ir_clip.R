5ad17cea65f9170626cdd87a370d3f6a002af233
henningte <34153309+henningte@users.noreply.github.com>
2020-02-12 15:41:09 +0100

    ir_clip can now handle multiple ranges at once.

--- R/ir_clip.R
+++ R/ir_clip.R
@@ -1,14 +1,17 @@
 #' Clips infrared spectra to new wavenumber ranges.
 #'
 #' \code{ir_clip} clips infrared spectra to a new, specified,
-#' wavenumber range.
+#' wavenumber range or multiple new specified wavenumber ranges.
 #'
 #' @param x An object of class \code{\link[ir:ir_new_ir]{ir}}.
-#' @param range A numeric vector of length two
-#' with the first element specifying the start
-#' of the new wavenumber range and the second
-#' element specifying the end of the new wavenumber
-#' range.
+#' @param range A \code{data.frame} with two columns and a
+#' row for each wavenumber range to keep. The columns are:
+#' \describe{
+#'   \item{start}{A numeric vector with start values for wavenumber ranges.}
+#'   \item{end}{A numeric vector with end values for wavenumber ranges.}
+#' }
+#' If \code{range} has more than one row, multiple ranges are clipped from
+#' \code{x} and merged together. Overlapping ranges are not allowed.
 #' @return An object of class \code{ir}.
 #' @export
 ir_clip <- function(x,
@@ -18,19 +21,27 @@ ir_clip <- function(x,
   if(!inherits(x, "ir")) {
     rlang::abort(paste0("`x` must be of class ir, not ", class(x)[[1]],"."))
   }
-  if(!is.numeric(range)) {
-    rlang::abort("`range` must be a numeric vector.")
+  if(!inherits(range, "data.frame")) {
+    rlang::abort("`range` must be a data.frame.")
   }
-  if(length(range) != 2) {
-    stop("`range` must be of length 2.")
+  if(ncol(range) != 2) {
+    rlang::abort("`range` must have two columns.")
+  }
+  range_check <- apply(range, 1, function(x) {
+    x[[1]] >= x[[2]]
+  })
+  if(any(range_check)) {
+    rlang::abort(paste0("For each row in `range`, `range$start` must be smaller than `range$end`. This is not the case for row(s) ", which(range_check), "."))
   }
 
-  range <- sort(range)
+  range <- range[order(range[, 1, drop = TRUE], decreasing = FALSE), ]
 
   # detect the corresponding row indices
   x_flat <- ir_flatten(x)
-  range <- ir_get_wavenumberindex(x_flat, wavenumber = range, warn = TRUE)
-  index <- range[1]:range[2]
+  range_nrow <- nrow(range)
+  range <- ir_get_wavenumberindex(x_flat, wavenumber = as.matrix(range), warn = TRUE)
+  range <- matrix(range, byrow = FALSE, nrow = range_nrow)
+  index <- unlist(apply(range, 1, function(x) x[[1]]:x[[2]]))
 
   # clip x
   x$spectra <- ir_stack(x_flat[index, ])$spectra

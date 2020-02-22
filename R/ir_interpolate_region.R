35dda0d38076091ffa7429d8f0a8415ffc804909
henningte <34153309+henningte@users.noreply.github.com>
2020-02-22 08:56:10 +0100

    ir_interpolate_region() now takes a data.frame to define regions to interpolate linearly.

--- R/ir_interpolate_region.R
+++ R/ir_interpolate_region.R
@@ -4,50 +4,57 @@
 #' region in infrared spectra.
 #'
 #' @param x An object of class \code{\link[ir:ir_new_ir]{ir}}.
-#' @param region A numerical vector of length two with the first value
-#' representing the start (smaller) wavenumber value of the region and the
-#' second value representing the end (larger) wavenumber value of the region
-#' to interpolate.
-#' @return \code{x} with the defined wavenumber region interpolated linearly.
+#' @param range A \code{data.frame} with a row for each region to interpolate
+#' linearly and two columns:
+#' \describe{
+#'   \item{start}{A numeric vector with start values for regions to interpolate
+#'   linearly (x axis values).}
+#'   \item{end}{A numeric vector with end values for regions to interpolate
+#'   linearly (x axis values).}
+#' }
+#' For each row in \code{range}, the values in \code{range$start} have to be
+#' smaller than the values in \code{range$end}.
+#' @return \code{x} with the defined wavenumber region(s) interpolated linearly.
 #' @export
 ir_interpolate_region <- function(x,
-                                  region) {
+                                  range) {
 
   ir_check_ir(x)
-  if(!is.numeric(region)) {
-    rlang::abort("`region` must be numeric, not ", class(region)[[1]], ".")
+  if(!inherits(range, "data.frame")) {
+    rlang::abort("`range` must be a data.frame.")
   }
-  if(length(region) != 2) {
-    rlang::abort("`region` must be of length 2, not ", length(region), ".")
+  if(ncol(range) != 2) {
+    rlang::abort("`range` must have two columns.")
   }
+  range_check <- apply(range, 1, function(x) {
+    x[[1]] >= x[[2]]
+  })
+  if(any(range_check)) {
+    rlang::abort(paste0("For each row in `range`, `range$start` must be smaller than `range$end`. This is not the case for row(s) ", which(range_check), "."))
+  }
+
+  range <- range[order(range[, 1, drop = TRUE], decreasing = FALSE), ]
 
   x_flat <- ir_flatten(x)
   index <- ir_get_wavenumberindex(x_flat,
-                                  region,
+                                  wavenumber = as.matrix(range),
                                   warn = TRUE)
+  index <- matrix(index, byrow = FALSE, nrow = nrow(range))
+  x_ranges <- apply(index, 1, function(x) x[[1]]:x[[2]])
 
-  x_range <- index[1]:index[2]
+  for(index in x_ranges) {
 
-  intensity_missing <- purrr::map_lgl(x_flat[, -1, drop = FALSE], function(y){
-    d <- data.frame(y = y[index], z = x_flat[index, 1, drop = TRUE])
-    all(is.na(d$y))})
-  if(any(intensity_missing)) {
-    rlang::warn(if(sum(intensity_missing) == 1) {
-      paste0("Spectrum ", which(intensity_missing), " has only NA values at the start and end of `region` and therefore was not inerpolated.")
-    }else {
-      paste0("Spectra ", which(intensity_missing), " have only NA values at the start and end of `region` and therefore were not inerpolated.")
+    x_flat[index, -1] <- purrr::map_df(x_flat[, -1, drop = FALSE], function(y){
+      d <- data.frame(y = y[index], z = x_flat[index, 1, drop = TRUE])
+      if(all(is.na(d$y))) {
+        rep(NA_real_, length(index))
+      } else {
+        m <- stats::lm(y ~ z, data = d[c(1, nrow(d)), , drop = FALSE])
+        stats::predict(m, newdata = d)
+      }
     })
-  }
 
-  x_flat[x_range, -1] <- purrr::map_df(x_flat[, -1, drop = FALSE], function(y){
-    d <- data.frame(y = y[index], z = x_flat[index, 1, drop = TRUE])
-    if(all(is.na(d$y))) {
-      rep(NA_real_, length(x_range))
-    } else {
-      m <- stats::lm(y ~ z, data = d)
-      stats::predict(m, newdata = data.frame(z = x_flat[x_range, 1, drop = TRUE]))
-    }
-  })
+  }
 
   x$spectra <- ir_stack(x_flat)$spectra
   x
